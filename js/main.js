95b488962e396d59df5f9d9da39d01453e34b51c
Pinknoisecc <allancast.c@gmail.com>
2023-03-15 18:00:46 -0300

    Animaciones y correcciones

---
+++ js/main.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.animacion2', { origin: 'top' });
+ScrollReveal().reveal('.animacion3', { origin: 'bottom' });
+ScrollReveal().reveal('.animacion1', { origin: 'left' });
+ScrollReveal().reveal('.animacion4', { origin: 'right' });
